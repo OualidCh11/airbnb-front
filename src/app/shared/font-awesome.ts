7af69de302b61c53989a22f5d3ef2bd70edd606c
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-07-26 10:18:26 +0100

    nav-v3

--- src/app/shared/font-awesome.ts
+++ src/app/shared/font-awesome.ts
@@ -1,9 +1,41 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faChessRook, faCircle, faEye, faLightbulb, faSnowflake, faTrashCan, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faBan, faBars, faBox, faChampagneGlasses, faCircleNotch, faDollarSign, faEarthEurope, faExclamation, faFire, faHillRockslide, faHouse, faKitchenSet, faMinus, faMugSaucer, faPersonSkiing, faPlus, faSailboat, faSearch, faTent, faTowerObservation, faTractor, faTree, faUmbrellaBeach, faWater } from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons:IconDefinition[] = [
     faAirbnb,
-    faUser
+    faUser,
+    faSearch,
+    faBars,
+    faBan,
+  faBox,
+  faChampagneGlasses,
+  faChessRook,
+  faCircle,
+  faCircleNotch,
+  faDollarSign,
+  faEarthEurope,
+  faExclamation,
+  faEye,
+  faFire,
+  faHillRockslide,
+  faHouse,
+  faKitchenSet,
+  faLightbulb,
+  faMinus,
+  faMugSaucer,
+  faPersonSkiing,
+  faPlus,
+  faSailboat,
+  faSnowflake,
+  faTent,
+  faTowerObservation,
+  faTractor,
+  faTrashCan,
+  faTree,
+  faUmbrellaBeach,
+  faWater,
+
 
 ]
