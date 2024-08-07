323fca875a38e863a1761185644965ae05c5824b
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-07 16:58:20 +0100

    creation user and state model

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import {HttpClient, HttpParams, HttpStatusCode} from "@angular/common/http";
+import {Location} from "@angular/common";
+import {Observable} from "rxjs";
+import { User } from '../../core/model/user.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  http = inject(HttpClient);
+  location = inject(Location);
+  notConnected = "Not_Connected";
+
+  private fetchUser$: WritableSignal<State<User>> =
+    signal(State.Builder<User>().forSuccess({email: this.notConnected}));
+  fetchUser = computed(() => this.fetchUser$());
 }
