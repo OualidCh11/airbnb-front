7fdbdb3b58139f1ff7b0a718d2d2a340c1ce5a09
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-07 17:34:04 +0100

    modify auth service and model

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {Location} from "@angular/common";
 import {Observable} from "rxjs";
 import { User } from '../../core/model/user.model';
 import { State } from '../../core/model/state.model';
+import { environment } from '../../../environment/environment';
 
 
 
@@ -32,5 +33,47 @@ export class AuthService {
           }
         }
       })
+  }
+
+  login(): void {
+    location.href = `${location.origin}${this.location.prepareExternalUrl("oauth2/authorization/okta")}`;
+  }
+
+
+  logout(): void {
+    this.http.post(`${environment.API_URL}/auth/logout`, {})
+      .subscribe({
+        next: (response: any) => {
+          this.fetchUser$.set(State.Builder<User>()
+            .forSuccess({email: this.notConnected}));
+          location.href = response.logoutUrl
+        }
+      })
+  }
+
+
+  isAuthenticated(): boolean {
+    if (this.fetchUser$().value) {
+      return this.fetchUser$().value!.email !== this.notConnected;
+    } else {
+      return false;
+    }
+  }
+
+  fetchHttpUser(forceResync: boolean): Observable<User> {
+    const params = new HttpParams().set('forceResync', forceResync);
+    return this.http.get<User>(`${environment.API_URL}/auth/get-authenticated-user`, {params})
+  }
+
+
+  hasAnyAuthority(authorities: string[] | string): boolean {
+    if(this.fetchUser$().value!.email === this.notConnected) {
+      return false;
+    }
+    if(!Array.isArray(authorities)) {
+      authorities = [authorities];
+    }
+    return this.fetchUser$().value!.authorities!
+      .some((authority: string) => authorities.includes(authority));
   }
 }
