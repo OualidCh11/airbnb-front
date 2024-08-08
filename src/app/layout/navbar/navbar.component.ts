75a7a89e9e017ffa33fb94c8c793d64c5d914c3d
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-08 10:18:34 +0100

    nav-comp-ts add hastoBeLandlord construcor modification

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
 import {  MenuModule  } from 'primeng/menu';
@@ -8,6 +8,8 @@ import { AvatarComponent } from './avatar/avatar.component';
 import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
 import { MenuItem } from 'primeng/api';
 import { ToastService } from '../../service/toastService/toast.service';
+import { AuthService } from '../../service/auth/auth.service';
+import { User } from '../../core/model/user.model';
 
 
 
@@ -35,26 +37,78 @@ export class NavbarComponent implements OnInit {
   dates = "Any week";
 
   toastService = inject(ToastService);
+  authService = inject(AuthService);
+ 
+
+  login = () => {
+    this.authService.login();
+  }
+
+  logout = () => {
+    this.authService.logout();
+  }
 
   currentMenuItems: MenuItem[] | undefined = []
+  connectedUser: User = {email: this.authService.notConnected}
+
+  constructor() {
+     effect(() => {
+        if(this.authService.fetchUser().status === "OK"){
+          this.connectedUser = this.authService.fetchUser().value!
+          this.currentMenuItems = this.fetchMenu();
+        }
+     })
+   }
 
   ngOnInit(): void {
-    this.currentMenuItems = this.fetchMenu();
-    this.toastService.send({severity:"info", summary:"Welcome to your home hehe"});
-
+    this.authService.fetch(false);
   }
 
   private fetchMenu(): MenuItem[] {
-    return[
-      {
-        label:"Sign up",
-        styleClass:"font-bold"
-      }, 
-      {
-        label:"Log in",
-      }
-    ]
+
+    if(this.authService.isAuthenticated()){
+      return[
+        {
+          label: "My properties",
+          routerLink: "landlord/properties",
+          visible: this.hastoBeLandlord(),
+        },
+        {
+          label: "My booking",
+          routerLink: "booking",
+        },
+        {
+          label: "My reservation",
+          routerLink: "landlord/reservation",
+          visible: this.hastoBeLandlord(),
+        },
+        {
+          label: "Log out",
+          command: this.logout
+        },
+      ]
+    }else{
+      return[
+        {
+          label:"Sign up",
+          styleClass:"font-bold",
+          command: this.login
+        }, 
+        {
+          label:"Log in",
+          command: this.login
+        }
+      ]
+    }
+    
   }
+
+  hastoBeLandlord(): boolean {
+    return this.authService.hasAnyAuthority("ROLE_LANDLORD")
+  }
+
+  
+  
   
 
 }
